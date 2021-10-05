4b0f8068cda4fd0d847ede54f9056146e536c179
Rudradanak <91957974+Rudradanak@users.noreply.github.com>
2021-10-05 18:30:11 +0530

    Create Rudra4.py

---
+++ Rudra4.py
@@ -0,0 +1,20 @@
+ # Take a list of numbers
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+# display the result
+print("Numbers divisible by 13 are",result)
+ # Python Program to find the factors of a number
+
+# This function computes the factor of the argument passed
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)
